40de495ff0cd8ab86e821015775841385edc6049
dzhani ivanov <djani.ivanov94@gmail.com>
2021-12-18 16:23:49 +0200

    updated questions

---
+++ src/questions.js
@@ -0,0 +1,334 @@
+const data = [
+  {
+    id: 1,
+    question: "What does AD stand for in relation to Windows Operating Systems?",
+    answers: [
+      {
+        text: "Alternative Drive",
+        correct: false,
+      },
+      {
+        text: "Automated Database",
+        correct: true,
+      },
+      {
+        text: "Active Department",
+        correct: false,
+      },
+      {
+        text: "Active Directory",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "The Harvard architecture for micro-controllers added which additional bus?",
+    answers: [
+      {
+        text: "Instruction",
+        correct: true,
+      },
+      {
+        text: "Adress",
+        correct: false,
+      },
+      {
+        text: "Control",
+        correct: false,
+      },
+      {
+        text: "Data",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 3,
+    question: "What is the most preferred image format used for logos in the Wikimedia database?",
+    answers: [
+      {
+        text: ".svg",
+        correct: true,
+      },
+      {
+        text: ".png",
+        correct: false,
+      },
+      {
+        text: ".jpeg",
+        correct: false,
+      },
+      {
+        text: ".gif",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 4,
+    question: "What did the name of the Tor Anonymity Network orignially stand for?",
+    answers: [
+      {
+        text: "The Only Router",
+        correct: false,
+      },
+      {
+        text: "The Ominous Router",
+        correct: false,
+      },
+      {
+        text: "The Onion Router",
+        correct: true,
+      },
+      {
+        text: "The Orange Router",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 5,
+    question: "What was the first commerically available computer processor?",
+    answers: [
+      {
+        text: "Intel 4004",
+        correct: true,
+      },
+      {
+        text: "TMS 1000",
+        correct: false,
+      },
+      {
+        text: "Intel 486SX",
+        correct: false,
+      },
+      {
+        text: "AMD AM386",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 6,
+    question: "What does the term GPU stand for?",
+    answers: [
+      {
+        text: "Gaming Processor Unit",
+        correct: true,
+      },
+      {
+        text: "Graphite Producing Unit",
+        correct: false,
+      },
+      {
+        text: "Graphical Proprietary Unit",
+        correct: false,
+      },
+      {
+        text: "Graphics Processing Unit",
+        correct: true,
+      }
+    ]
+  },
+  {
+    id: 7,
+    question: "Which of these is not a key value of Agile software development?",
+    answers: [
+      {
+        text: "Individuals and interactions",
+        correct: false,
+      },
+      {
+        text: "Comprehensive documentation",
+        correct: true,
+      },
+      {
+        text: "Responding to change",
+        correct: false,
+      },
+      {
+        text: "Customer collaboration",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 8,
+    question: "The acronym &quot;RIP&quot; stands for which of these?",
+    answers: [
+      {
+        text: "Runtime Instance Processes",
+        correct: false,
+      },
+      {
+        text: "Regular Interval Processes",
+        correct: false,
+      },
+      {
+        text: "Routine Inspection Protocol",
+        correct: false,
+      },
+      {
+        text: "Routing Information Protocol",
+        correct: true,
+      }
+    ]
+  },
+  {
+    id: 9,
+    question: "What is the number of keys on a standard Windows Keyboard?",
+    answers: [
+      {
+        text: "104",
+        correct: true,
+      },
+      {
+        text: "64",
+        correct: false,
+      },
+      {
+        text: "94",
+        correct: false,
+      },
+      {
+        text: "76",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 10,
+    question: "What amount of bits commonly equals one byte?",
+    answers: [
+      {
+        text: "1",
+        correct: false,
+      },
+      {
+        text: "8",
+        correct: true,
+      },
+      {
+        text: "2",
+        correct: false,
+      },
+      {
+        text: "64",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 11,
+    question: "What was the name given to Android 4.3?",
+    answers: [
+      {
+        text: "Lollipop",
+        correct: false,
+      },
+      {
+        text: "Nutella",
+        correct: false,
+      },
+      {
+        text: "Froyo",
+        correct: false,
+      },
+      {
+        text: "Jelly Bean",
+        correct: true,
+      }
+    ]
+  },
+  {
+    id: 12,
+    question: "While Apple was formed in California, in which western state was Microsoft founded?",
+    answers: [
+      {
+        text: "Washington",
+        correct: false,
+      },
+      {
+        text: "New Mexico",
+        correct: true,
+      },
+      {
+        text: "Colorado",
+        correct: false,
+      },
+      {
+        text: "Arizona",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 13,
+    question: "What port does HTTP run on?",
+    answers: [
+      {
+        text: "80",
+        correct: true,
+      },
+      {
+        text: "53",
+        correct: false,
+      },
+      {
+        text: "443",
+        correct: false,
+      },
+      {
+        text: "23",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 14,
+    question: "Which RAID array type is associated with data mirroring?",
+    answers: [
+      {
+        text: "RAID 0",
+        correct: false,
+      },
+      {
+        text: "RAID 1",
+        correct: true,
+      },
+      {
+        text: "RAID 5",
+        correct: false,
+      },
+      {
+        text: "RAID 10",
+        correct: false,
+      }
+    ]
+  },
+  {
+    id: 15,
+    question: "In computing, what does LAN stand for?",
+    answers: [
+      {
+        text: "Long Antenna Node",
+        correct: false,
+      },
+      {
+        text: "Light Access Node",
+        correct: false,
+      },
+      {
+        text: "Local Area Network",
+        correct: true,
+      },
+      {
+        text: "Land Address Navigation",
+        correct: false,
+      }
+    ]
+  },
+];
+
+export default data;
